1cfc80a1352a66b683bc541c48e6a33b3eac5412
bugg86 <jdfink@uncg.edu>
2021-02-15 17:20:56 -0500

    Updated call in Main.java
    Got rid of the constructor in CovidDataAPI.java, and just called the static method Main.java.

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,5 @@
 public class Main {
     public static void main(String[] args){
-        CovidDataAPI dataAPI = new CovidDataAPI();
-        dataAPI.getCovidData();
+        CovidDataAPI.getCovidData();
     }
 }
